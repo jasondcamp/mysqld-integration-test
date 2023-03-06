cf076b949e3756c3202ee91c83aebd07b54f957d
Jason Camp <jason@readyset.io>
2023-03-06 16:45:02 -0500

    Updated tests

--- tests/test_integration_mariadb.py
+++ tests/test_integration_mariadb.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.mark.slow
 @pytest.mark.integration_test
 def test_mysqld_run_mariadb():
-    mysqld = Mysqld(mysqld_binary='mariadb-10.11.2-linux-systemd-x86_64/bin/mysqld',
-                    mysql_install_db_binary='mariadb-10.11.2-linux-systemd-x86_64/scripts/mysql_install_db')
+    mysqld = Mysqld(mysqld_binary='data/mariadb-10.11.2-linux-systemd-x86_64/bin/mysqld',
+                    mysql_install_db_binary='data/mariadb-10.11.2-linux-systemd-x86_64/scripts/mysql_install_db')
     instance = mysqld.run()
     assert instance.username == 'root'
