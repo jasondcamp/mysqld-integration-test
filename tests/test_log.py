19d8b0e1bd124dc4b4caafc3114207e1e0427bd7
Jason Camp <jason@readyset.io>
2023-03-05 20:39:09 -0500

    Added mysqld tests for mysql and mariadb, fixed some missing test code

--- tests/test_log.py
+++ tests/test_log.py
@@ -10,6 +10,30 @@ def test_log_levelset_noexception():
 
 
 @pytest.mark.log_test
+def test_log_levelset_info_noexception():
+    logger.setlevel('INFO')
+    assert True
+
+
+@pytest.mark.log_test
+def test_log_levelset_debug_noexception():
+    logger.setlevel('DEBUG')
+    assert True
+
+
+@pytest.mark.log_test
+def test_log_levelset_error_noexception():
+    logger.setlevel('ERROR')
+    assert True
+
+
+@pytest.mark.log_test
+def test_log_levelset_warn_noexception():
+    logger.setlevel('WARN')
+    assert True
+
+
+@pytest.mark.log_test
 def test_log_levelset_fail():
     with pytest.raises(InvalidLogLevel):
         logger.setlevel('FAKE')
--- tests/test_mysqld.py
+++ tests/test_mysqld.py
@@ -1,12 +1,37 @@
 from mysqld_integration_test import Mysqld
+from mysqld_integration_test.settings import ConfigFile
 import os
 import pytest
-
+import pytest_mock
+from mock import patch
+import mock
+#from unittest.mock import patch
 
 @pytest.mark.mysqld_test
 def test_mysqld_init():
     mysqld = Mysqld()
     assert mysqld.base_dir is not None
+
+@pytest.mark.mysqld_test
+def test_mysqld_run_mariadb():
+    mysqld = Mysqld()
+    mysqld.config.variant = "mariadb"
+    mysqld.config.version_major = 10
+    mysqld.config.version_minor = "5.16"
+
+    instance = mysqld.run()
+    assert instance.username == 'root'
+
+
+@pytest.mark.mysqld_test
+def test_mysqld_run_mysql():
+    mysqld = Mysqld()
+    mysqld.config.variant = "mysql"
+    mysqld.config.version_major = 8
+    mysqld.config.version_minor = "0.30"
+
+    instance = mysqld.run()
+    assert instance.username == 'root'
 
 
 @pytest.mark.mysqld_test
