a89ffce51066a4f54d86c54bca6b72139bbb3bd9
Jason Camp <me@jasoncamp.com>
2023-12-10 16:59:40 -0500

    Missed open ticket, added to regexp for mysql version parsing

--- mysqld_integration_test/helpers.py
+++ mysqld_integration_test/helpers.py
@@ -26,7 +26,7 @@ class Utils():
 
     @staticmethod
     def parse_version(version_str):
-        version_info = (re.findall(r"Ver ([0-9.]+)\-?([a-zA-Z0-9.-]+)? for", version_str))
+         version_info = (re.findall(r"Ver ([0-9.]+)\-?([+a-zA-Z0-9.-]+)? for", version_str))
         if version_info:
             (version_major, version_minor) = version_info[0][0].split('.', 1)
             version_major = int(version_major)
